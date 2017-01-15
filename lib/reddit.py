7f6a13cab3ad3e2082f32232800461178f2889f8
Bryan Neal Garrison <bryangarrison@me.com>
2017-01-15 16:19:48 -0500

    converted stray tabs to spaces and moved section to reading from config file

--- lib/reddit.py
+++ lib/reddit.py
@@ -8,45 +8,51 @@ import os
 
 class Reddit():
 
-	def __init__(self, configObj):
-		self.config = configObj
-		self.configDir = os.path.expanduser("~") + '/.config/shower_thoughts'
+    def __init__(self, configObj):
+        self.config = configObj
+        self.configDir = os.path.expanduser("~") + '/.config/shower_thoughts'
 
 
-	def pull(self):
+    def pull(self):
 
-		url = "https://www.reddit.com/r/showerthoughts/top.json?t={}&limit={}".format(self.config.timeframe, self.config.limit)
+        url = "https://www.reddit.com/r/{}/{}.json?t={}&limit={}"\
+            .format(
+                self.config.subreddit,
+                self.config.section,
+                self.config.timeframe,
+                self.config.limit
+            )
 
-		output = requests.get(url).json()
+        output = requests.get(url).json()
 
-		if 'error' in output.keys():
-			print(output['message'])
+        if 'error' in output.keys():
+            print(output['message'])
 
-		else:
-			if os.path.exists(self.configDir):
-				with open(self.configDir + '/database.json', 'w') as database:
-					json.dump(output, database)
-
-
-	def read(self):
-		if os.path.exists(self.configDir + '/database.json'):
-			with open(self.configDir + '/database.json', 'r') as database:
-				data = json.loads(database.read())
-
-			self.thoughts = []
-
-			data = data['data']['children']
-			for x in data:
-				self.thoughts.append(thought.Thought(x['data']))
-		else:
-			raise Exception("No database to pull from, run 'shower_thoughts update' first to pull from reddit.")
+        else:
+            if os.path.exists(self.configDir):
+                with open(self.configDir + '/database.json', 'w') as database:
+                    json.dump(output, database)
 
 
-	def randomThought(self):
-		thought = choice(self.thoughts)
+    def read(self):
+        if os.path.exists(self.configDir + '/database.json'):
+            with open(self.configDir + '/database.json', 'r') as database:
+                data = json.loads(database.read())
 
-		if os.path.exists(self.configDir):
-			with open(self.configDir + '/lastDisplayed.json', 'w') as last:
-				json.dump(thought.jsonExport(), last)
+            self.thoughts = []
 
-		return thought
+            data = data['data']['children']
+            for x in data:
+                self.thoughts.append(thought.Thought(x['data']))
+        else:
+            raise Exception("No database to pull from, run 'shower_thoughts update' first to pull from reddit.")
+
+
+    def randomThought(self):
+        thought = choice(self.thoughts)
+
+        if os.path.exists(self.configDir):
+            with open(self.configDir + '/lastDisplayed.json', 'w') as last:
+                json.dump(thought.jsonExport(), last)
+
+        return thought
