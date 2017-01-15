e1030846530af8a692332ad4254636ca99b713e5
Bryan Neal Garrison <bryangarrison@me.com>
2017-01-15 16:16:53 -0500

    converted stray tabs to spaces

--- lib/thought.py
+++ lib/thought.py
@@ -1,27 +1,27 @@
 class Thought():
 
-	def __init__(self, data):
-		self.text = data['title']
-		self.subtext = data['selftext']
-		self.author = data['author']
-		self.permalink = "https://reddit.com" + data['permalink']
-		self.score = data['score']
-		self.created = data['created']
-		self.nsfw = data['over_18']
-		self.id = data['id']
+    def __init__(self, data):
+        self.text = data['title']
+        self.subtext = data['selftext']
+        self.author = data['author']
+        self.permalink = "https://reddit.com" + data['permalink']
+        self.score = data['score']
+        self.created = data['created']
+        self.nsfw = data['over_18']
+        self.id = data['id']
 
 
-	def __str__(self):
-		return self.text + '\n -' + self.author
+    def __str__(self):
+        return self.text + '\n -' + self.author
 
 
-	def __repr__(self):
-		return "Thought: " + self.id
+    def __repr__(self):
+        return "Thought: " + self.id
 
-	def jsonExport(self):
-		return {
-			'title':     self.text,
-			'author':    self.author,
-			'permalink': self.permalink,
-			'id':        self.id
-		}
+    def jsonExport(self):
+        return {
+            'title':     self.text,
+            'author':    self.author,
+            'permalink': self.permalink,
+            'id':        self.id
+        }
