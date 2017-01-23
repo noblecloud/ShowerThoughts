2433238d3d21b75de416b8baabd273a755485fa3
Bryan Neal Garrison <bryangarrison@me.com>
2017-01-23 15:03:48 -0500

    Somewhat working DocOpt

--- shower_thoughts.py
+++ shower_thoughts.py
@@ -1,40 +1,48 @@
 #!/usr/local/bin/python3
 
-from lib import config
-from lib import thought
-from lib import reddit
-import sys
+"""Shower Thoughts
 
+Usage:
+  shower_thoughts
+  shower_thoughts update
+  shower_thoughts open
+  shower_thoughts config
+
+Options:
+  -h --help     Show this screen.
+"""
+
+from docopt import docopt
+import lib
 
 class ShowerThoughts():
 
 
-    def __init__(self):
+    def __init__(self, args):
 
-        self.config = config.Config()
+        self.config = lib.Config()
 
-        self.connection = reddit.Reddit(self.config)
+        self.connection = lib.Reddit(self.config)
 
         # description='Displays a random shower thought from reddit',
-                    usage='''shower_thought <command> [<args>]
+           #          usage='''shower_thought <command> [<args>]
+           #
+           # update       Update local database of shower thoughts
+           # open         Open comments section of last quote
+           # conf         Get or set setting in config file''')
 
-           update       Update local database of shower thoughts
-           open         Open comments section of last quote
-           conf         Get or set setting in config file''')
-
-        # parser.add_argument('command',
-            help='Subcommand to run [update|conf|open]', nargs='*')
+        if args['update']:
+            self.update()
 
     def read(self):
         try:
             self.connection.read()
         except Exception:
-            self.connection.pull()
-            self.read()
+            print("Unable to read database.  Try running \"shower_thoughts update\"")
 
     def update(self):
         self.connection.pull()
-
+        print("Updated")
 
     def conf(self):
         import pprint
@@ -52,10 +60,13 @@ class ShowerThoughts():
                 webbrowser.open(x['permalink'], new=0, autoraise=True)
 
 
-        # with open(self.configDir + '/config.json', 'r') as configFile:
-        #     self.config = json.loads(configFile.read())
-
+        with open(self.configDir + '/config.json', 'r') as configFile:
+            self.config = json.loads(configFile.read())
 
 
 if __name__ == '__main__':
-    ShowerThoughts()
+
+    arguments = docopt(__doc__, version='Shower Thoughts')
+    print(arguments)
+    ShowerThoughts(arguments)
+
